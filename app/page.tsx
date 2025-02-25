9ab3ce32068f2635bbe0402a41f565f1486f6c63
Ray <rlawlsdn9583@gmail.com>
2025-02-25 23:26:31 +0900

    chore: 폰트 색상 설정

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,14 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <div>
+      <Button className="text-md text-ELSE-CC6">
+        프로젝트
+        프로젝트
+        프로젝트
+
+      </Button>
     </div>
   );
 }
