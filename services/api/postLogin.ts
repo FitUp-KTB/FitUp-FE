2bf887c76c5f8cd2b36980a68013c98ef97a1009
HyunWoo Jeong <jrwedo@naver.com>
2025-02-27 11:03:11 +0900

    Refactor: Base url 환경변수 정의해서 사용

--- services/api/postLogin.ts
+++ services/api/postLogin.ts
@@ -4,7 +4,7 @@ import {LoginRequest, LoginResponse} from "@/dto/loginDTO";
 const jsonContentType = {"content-type": "application/json"};
 
 const postLogin = async (reqBody: LoginRequest): Promise<BaseResponse<LoginResponse>> => {
-  const response = await fetch("https://vivi-o.site/api/v1/users/token", {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/users/token`, {
     method: "POST",
     headers: jsonContentType,
     body: JSON.stringify(reqBody)
