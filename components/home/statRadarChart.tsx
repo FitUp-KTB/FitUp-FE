3b935b27a649623573f294a16de4dc3d82f79490
HyunWoo Jeong <jrwedo@naver.com>
2025-02-27 14:39:21 +0900

    Feat: Radar, Line 차트에서 같은 stat 데이터를 사용하도록 구현

--- components/home/statRadarChart.tsx
+++ components/home/statRadarChart.tsx
@@ -1,30 +1,21 @@
 "use client";
 import {Radar} from "react-chartjs-2";
-import {Chart, Filler, LineElement, PointElement, RadialLinearScale} from "chart.js";
+import {Chart, ChartData, Filler, LineElement, PointElement, RadialLinearScale} from "chart.js";
 import {COLORS} from "@/styles/colors";
 import ChartDataLabels from 'chartjs-plugin-datalabels';
 import applyAlpha from "@/util/colorUtil";
+import {statAtom} from "@/store/statAtom";
+import {useAtomValue} from "jotai";
+import {useEffect, useState} from "react";
 
 Chart.register(RadialLinearScale, PointElement, LineElement, ChartDataLabels, Filler);
-
-const data = {
-  labels: ["Strength", "Endurance", "Speed", "Flexibility", "Stamina"],
-  datasets: [{
-    label: "유저 스탯",
-    data: [40, 50, 80, 70, 50],
-    fill: "start",
-    backgroundColor: applyAlpha(COLORS.PURPLE, 0.4),
-    borderColor: COLORS.PURPLE,
-    pointBackgroundColor: COLORS.PURPLE,
-  }]
-}
 
 const options = {
   responsive: true,
   maintainAspectRatio: false,
   scales: {
     r: {
-      angleLines: { display: true },
+      angleLines: {display: true},
       suggestedMin: 0,
       suggestedMax: 100,
       ticks: {
@@ -47,12 +38,36 @@ const options = {
 }
 
 export default function StatRadarChart() {
+  const stat = useAtomValue(statAtom);
+  const [chartData, setChartData] = useState<ChartData<'radar'>>({datasets: []})
+
+  useEffect(() => {
+    setChartData({
+      labels: ["Strength", "Endurance", "Speed", "Flexibility", "Stamina"],
+      datasets: [{
+        label: "유저 스탯",
+        data: [
+          stat?.strength ?? 0,
+          stat?.endurance ?? 0,
+          stat?.speed ?? 0,
+          stat?.flexibility ?? 0,
+          stat?.stamina ?? 0
+        ],
+        fill: "start",
+        backgroundColor: applyAlpha(COLORS.PURPLE, 0.4),
+        borderColor: COLORS.PURPLE,
+        pointBackgroundColor: COLORS.PURPLE,
+      }]
+    })
+  }, [stat]);
+
+
   return (
     <div className="bg-WHITE w-72 h-72 rounded-3xl shadow p-4 flex flex-col">
       <h2 className="text-xl font-bold text-gray-800">스탯 차트</h2>
 
       <div className="flex-1 relative ">
-        <Radar data={data} options={options} />
+        <Radar data={chartData} options={options}/>
       </div>
     </div>
   );
