831f1574f3d5f181d8987a9681415b406ce16202
HyunWoo Jeong <jrwedo@naver.com>
2025-02-27 10:56:02 +0900

    Refactor: api 마다 파일 분리

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -5,7 +5,7 @@ import {Input} from "@/components/ui/input";
 import {Button} from "@/components/ui/button";
 import {useRouter} from "next/navigation";
 import {LoginRequest} from "@/dto/loginDTO";
-import {postLogin} from "@/services/api";
+import {postLogin} from "@/services/api/postLogin";
 
 export default function LoginPage() {
   const router = useRouter();
