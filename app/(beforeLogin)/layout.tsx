1773aae57ccae0530844c24a08a35385dc44d637
HyunWoo Jeong <jrwedo@naver.com>
2025-04-12 20:22:31 +0900

    refactor: GradientBackground 파일명 카멜케이스로 변경

--- app/(beforeLogin)/layout.tsx
+++ app/(beforeLogin)/layout.tsx
@@ -1,5 +1,5 @@
 import {ReactNode} from "react";
-import GradientBackground from "@/app/(beforeLogin)/_component/GradientBackground";
+import GradientBackground from "@/app/(beforeLogin)/_component/gradientBackground";
 
 type Props = {children: ReactNode}
 export default function BeforeLoginLayout({children}: Props) {
